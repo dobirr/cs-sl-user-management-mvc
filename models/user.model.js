d6d1b1da31a684359cc68d24435aa6ddf2df80ab
Dennis Dobirr <dennis.dobirr@realcore.de>
2026-02-15 11:17:05 +0100

    feat(api): add user model

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      minlength: [2, 'Name must be at least 2 characters long'],
+      maxlength: [50, 'Name must be at most 50 characters long'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', UserSchema);
