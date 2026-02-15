dc8a582ae3bcd323ab277898b72ab98ae1ab794e
Dennis Dobirr <dennis.dobirr@realcore.de>
2026-02-15 13:46:31 +0100

    feat(api): modify middleware in app.js

--- app.js
+++ app.js
@@ -31,11 +31,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Routes
+app.use('/users', userRoutes);
+
 app.get('/', (req, res) => {
   res.redirect('/users');
 });
 
-app.use('/users', userRoutes);
+// 404 Handler
+app.use((req, res) => {
+  res.status(404).send(`
+  <h1>404 Not Found</h1><p>The page you are looking for does not exist</p><a href="/users">Go to Users</a>`);
+});
 
 
 // Start Server
@@ -44,4 +57,14 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
   console.log('================================');
+
+  // Available Routes
+  console.log('Available Routes:');
+  console.log(`GET http://localhost:${PORT}/users - List all users`);
+  console.log(`GET http://localhost:${PORT}/users/new - Form to create a new user`);
+  console.log(`POST http://localhost:${PORT}/users - Create a new user`);
+  console.log(`GET http://localhost:${PORT}/users/:id - View user details`);
+  console.log(`GET http://localhost:${PORT}/users/:id/edit - Form to edit user`);
+  console.log(`POST http://localhost:${PORT}/users/:id - Update user`);
+  console.log(`POST http://localhost:${PORT}/users/:id/delete - Delete user`);
 });
